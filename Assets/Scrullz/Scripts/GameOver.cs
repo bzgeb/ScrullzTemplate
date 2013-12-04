5cd96c5d2c2064ff81fb9a78394a1fe865ea0394
Bronson Zgeb <bronson.zgeb@gmail.com>
2013-12-03 22:22:08 -0500

    Added a win condition checker and a game over scene

---
+++ Assets/Scrullz/Scripts/GameOver.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+    public bool IsGameOver;
+    // Update is called once per frame
+    void Update () {
+        if ( IsGameOver ) {
+            Application.LoadLevel( "GameOver" );
+        }
+    }
+}
